e7f33314a508140a14333328ff947405e16c7336
francisco.pippi <f.pippi@edu.pucrs.br>
2025-09-01 14:30:16 -0300

    Commit

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Security
   app.use(helmet());
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  app.use(compression());
 
   // Global validation pipe
   app.useGlobalPipes(
